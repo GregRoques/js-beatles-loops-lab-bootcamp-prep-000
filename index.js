3765f15839a3db0f38ac7b49127f8852a9f1adc8
Greg Roques <greg@gregroques.com>
2018-09-01 17:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) { 
+var array =[];
+for(var i = 0; i<4;i++){
+  array.push(musicians[i] + " plays " + instruments[i]);
+}
+return array;
+} 
+
+function johnLennonFacts(facts){
+  var array = [];
+  var i = 0;
+  while(i<facts.length){
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  var i=0
+  function incrementVariable(){
+  i=i+1;}
+    do{
+   array.push("I love the Beatles!");
+   incrementVariable();} 
+ while(i<=n);
+return array;
+}
